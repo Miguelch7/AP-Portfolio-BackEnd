339b01ff477fdffe405d966ca7c8bb65f2d287a8
Miguel <chanampamiguel@gmail.com>
2022-07-21 19:38:01 -0300

    Configuración Inicial

---
+++ src/test/java/argentinaprograma/portfolioweb/PortfoliowebApplicationTests.java
@@ -0,0 +1,13 @@
+package argentinaprograma.portfolioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
