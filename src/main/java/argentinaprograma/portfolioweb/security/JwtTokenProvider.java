ce2216c9d84e1f30e7b56efe3c193070210571dd
Miguel <chanampamiguel@gmail.com>
2022-08-09 12:13:36 -0300

    Agregar roles en JWT

--- src/main/java/argentinaprograma/portfolioweb/security/JwtTokenProvider.java
+++ src/main/java/argentinaprograma/portfolioweb/security/JwtTokenProvider.java
@@ -6,9 +6,11 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtTokenProvider {
@@ -16,15 +18,19 @@ public class JwtTokenProvider {
     @Value("${app.jwt-secret}")
     private String jwtSecret;
 
+    @Value("${app.jwt-authorities-key}")
+    private String jwtAuthoritiesKey;
+
     @Value("${app.jwt-expiration-milliseconds}")
     private int jwtExpirationInMs;
 
     public String generarToken(Authentication authentication) {
         String username = authentication.getName();
+        String authorities = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
         Date fechaActual = new Date();
         Date fechaExpiracion = new Date(fechaActual.getTime() + jwtExpirationInMs);
 
-        String token = Jwts.builder().setSubject(username).setIssuedAt(new Date()).setExpiration(fechaExpiracion).signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+        String token = Jwts.builder().claim(jwtAuthoritiesKey, authorities).setSubject(username).setIssuedAt(new Date()).setExpiration(fechaExpiracion).signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
 
         return token;
     }
