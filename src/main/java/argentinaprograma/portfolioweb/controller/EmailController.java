56659e594d78001eb5bb837ca2aa382215ce3c05
Miguel <chanampamiguel@gmail.com>
2022-08-15 22:36:32 -0300

    CrossOrigin en /email/send

--- src/main/java/argentinaprograma/portfolioweb/controller/EmailController.java
+++ src/main/java/argentinaprograma/portfolioweb/controller/EmailController.java
@@ -5,6 +5,7 @@ import argentinaprograma.portfolioweb.service.IEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/email")
 public class EmailController {
